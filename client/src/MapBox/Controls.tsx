310501b58cb1f78ac27ac79cddd8ebca1a5fc082
Adam Bourg <adam.bourg@gmail.com>
2024-07-28 18:36:17 -0600

    adding a few buttons

--- client/src/MapBox/Controls.tsx
+++ client/src/MapBox/Controls.tsx
@@ -1,5 +1,6 @@
 import { Button, Container } from "@mui/material";
 import { styled } from '@mui/material/styles';
+import IosShareIcon from '@mui/icons-material/IosShare';
 
 const ControlButton = styled(Button)`
     margin-left: 1.2rem;
@@ -8,9 +9,9 @@ const ControlButton = styled(Button)`
 const Controls = () => {
     return (
         <Container sx={{padding: 1}}>
-            <ControlButton variant="outlined">Hello World</ControlButton>
-            <ControlButton variant="outlined">Hello World</ControlButton>
-            <ControlButton variant="outlined">Hello World</ControlButton>
+            <ControlButton variant="outlined">View All</ControlButton>
+            <ControlButton variant="outlined">Save</ControlButton>
+            <ControlButton variant="outlined"><IosShareIcon/></ControlButton>
         </Container>
     )
 }
