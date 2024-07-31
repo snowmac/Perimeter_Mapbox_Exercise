7e8a472921a6b5b794b5291a617a1c047c26d801
Adam Bourg <adam.bourg@gmail.com>
2024-07-31 16:38:58 -0600

    Revert "adding it as rest instead;"
    This reverts commit 9b4944865a0aad35f0abc543bf9e0a0b0e61a032.

--- server/index.js
+++ server/index.js
@@ -1,18 +1,1 @@
-import Fastify from 'fastify';
-import routes from './src/routes'; 
-
-const fastify = Fastify({ logger: true });
-
-fastify.register(routes);
-
-const start = async () => {
-    try {
-        await fastify.listen({ port: 3000 });
-        fastify.log.info(`Server is running at http://localhost:3000`);
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-};
-
-start();
+console.log('hello world')
--- server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance, FastifyPluginOptions } from 'fastify';
-import workSessionRoutes from './workSessionRoutes';
-import polygonRoutes from './polygonRoutes'; 
-
-
-export default async function (fastify: FastifyInstance, options: FastifyPluginOptions) {
-    fastify.register(workSessionRoutes);
-    fastify.register(polygonRoutes); 
-}
