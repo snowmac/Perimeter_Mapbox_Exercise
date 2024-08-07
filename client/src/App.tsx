86cf68fd8722d70c4645ca92e3e01fed9a745fa3
Adam Bourg <adam.bourg@gmail.com>
2024-08-07 00:31:33 -0600

    adding mutation to get session and get all polygons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,28 +5,36 @@ import MapBox from "@components/MapBox/MapBox.tsx";
 import Controls from "@components/Controls/Controls";
 import { queries, mutations } from "@src/graphql";
 import { useEffect, useState } from "react";
-import Cookies from 'js-cookie'; 
+import Cookies from "js-cookie";
 
+// should wrap all this logic into a service 
+// should wrap this out of app and into a separate HOC
 export default function App() {
-  const [sessionId, setSessionId] = useState('');
-  // TODO: Stuff cookie name into config 
+  const [sessionId, setSessionId] = useState("");
+  // TODO: Stuff cookie name into config
   // undefined or string id
-  const weHaveSessionCookie = Cookies.get('work-session-id');  
+  const weHaveSessionCookie = Cookies.get("work-session-id");
 
   const [initWorkSession] = useMutation(mutations.INIT_WORK_SESSION);
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data: {createSession: {id: sessionId}} } = await initWorkSession();
-      Cookies.set('work-session-id', sessionId);
+      const {
+        data: {
+          createSession: { id: sessionId },
+        },
+      } = await initWorkSession();
+      // TODO: wrap into something like a helper
+      const in30Minutes = 1 / 48;
+      Cookies.set("work-session-id", sessionId, { expires: in30Minutes });
       setSessionId(sessionId);
     };
 
     // undefined cookie, go get the data and set the session id
-    if(!weHaveSessionCookie) {
-      fetchData(); 
+    if (!weHaveSessionCookie) {
+      fetchData();
     } else {
-      setSessionId(weHaveSessionCookie)
+      setSessionId(weHaveSessionCookie);
     }
   }, []);
 
@@ -37,23 +45,23 @@ export default function App() {
   });
 
   const onCreate = (item: any) => {
-    alert('onCreate!');
+    alert("onCreate!");
     console.log(JSON.stringify(item));
     // const { data } = useMutation();
-    console.log(sessionId)
+    console.log(sessionId);
   };
   const onUpdate = (item: any) => {
-    alert('onUpdate!')
+    alert("onUpdate!");
     console.log(item);
   };
   const onDelete = (item: any) => {
-    alert('onDelete!')
+    alert("onDelete!");
     console.log(item);
   };
 
   const clearPoints = (item: any) => {};
   const onSave = (item: any) => {
-    alert('onSave!');
+    alert("onSave!");
     console.log(item);
   };
 
